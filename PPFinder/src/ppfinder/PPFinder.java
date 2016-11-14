d4b6355de4a1191dcf44b45aa3f13202a7388f46
DragonSlifer <dragonslifer45@gmail.com>
2016-11-14 15:09:01 +0100

    Commit Inicial
    Aplicacion funcionando de la segunda sesión. Hay que realizar una
    "separación" entre cliente y servidor de tal forma que se puedan
    conectar al menos dos dispositivos reales.

---
+++ PPFinder/src/ppfinder/PPFinder.java
@@ -0,0 +1,19 @@
+/*
+ *  
+ */
+package ppfinder;
+
+import view.MainFrame;
+
+public class PPFinder {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Iniciando Programa");
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+    }
+    
+}
